b840a7bce6992c024c6e8f3f4b5559e81adc02d3
Wahaj <100323687+wahajals@users.noreply.github.com>
2022-08-25 04:50:43 +0300

    added

--- FavMovie/FavMovie/Model/Movie.swift
+++ FavMovie/FavMovie/Model/Movie.swift
@@ -17,7 +17,7 @@ struct Movie: Identifiable {
 let myMovies =
     [
         Movie(MovieName: "Scent Of A Woman", MovieAct: ["Al Pacino","Chris O'Donnell", "Gabrielle Anwar"], MovieStory: "the story of a preparatory school student who takes a job as an assistant to an irritable, blind, medically retired Army lieutenant colonel"),
-        Movie(MovieName: "The Curious Case Of Benjamin", MovieAct: ["Brad Pitt", "Kate Blanchett"], MovieStory: "Tells the story of Benjamin Button, a man who starts aging backwards with consequences"),
+        Movie(MovieName: "The Curious Case Of Benjamin", MovieAct: ["Brad Pitt", "Cate Blanchett"], MovieStory: "Tells the story of Benjamin Button, a man who starts aging backwards with consequences"),
         Movie(MovieName: "Roman Holiday", MovieAct: ["Gregory Peck", "Audrey Hepburn"],MovieStory: "A bored and sheltered princess escapes her guardians and falls in love with an American newsman in Rome"),
         Movie(MovieName: "Titanic", MovieAct: ["Leonardo DiCaprio", "Kate Winslet", "Billy Zane"],MovieStory: "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic"),
         Movie(MovieName: "Gone With The Wind", MovieAct: ["Clark Gable", "Vivien Leigh"],MovieStory: "The manipulative daughter of a Georgia plantation owner conducts a turbulent romance with a roguish profiteer during the American Civil War and Reconstruction periods"),
